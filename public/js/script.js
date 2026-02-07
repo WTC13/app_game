cb1b50d1216a3191ee618ef710c12e0dccccf2ee
Wendell Costa <wendell.timoteoc@gmail.com>
2026-02-06 23:02:50 -0300

    update project - create page home.html

--- public/js/script.js
+++ public/js/script.js
@@ -11,3 +11,21 @@ $(document).ready(function() {
         alert('Login enviado! Verifique o console.');
     });
 });
+
+$(document).ready(function() {
+    // Interação de Checkbox para Missões
+    $('input[type="checkbox"]').on('change', function() {
+        if($(this).is(':checked')) {
+            $(this).parent().css('text-decoration', 'line-through');
+            $(this).parent().css('opacity', '0.5');
+            // Aqui entraria a chamada para o seu backend Node.js
+            console.log("XP Ganho com sucesso!");
+        } else {
+            $(this).parent().css('text-decoration', 'none');
+            $(this).parent().css('opacity', '1');
+        }
+    });
+
+    // Simulação de pulso na barra de XP ao carregar
+    $('.xp-bar').hide().fadeIn(1500);
+});
